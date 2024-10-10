f591953e3f75a6c64f1b258a12d97e5bff5639cc
Jeevan-Neupane <jeevan.neupane003@gmail.com>
2024-10-10 18:36:26 +0545

    my projects addded

---
+++ src/data/Projects.ts
@@ -0,0 +1,20 @@
+const MyProjects = [
+    {
+        name: "Project 1",
+        description: "This is a project",
+        image: "https://via.placeholder.com/150",
+        link: "https://www.google.com"
+    },
+    {
+        name: "Project 2",
+        description: "This is a project",
+        image: "https://via.placeholder.com/150",
+        link: "https://www.google.com"
+    },
+    {
+        name: "Project 3",
+        description: "This is a project",
+        image: "https://via.placeholder.com/150",
+        link: "https://www.google.com"
+    }
+]
